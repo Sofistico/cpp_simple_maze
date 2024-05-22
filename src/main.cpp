5552ca5ffe8ce15ed4ee22e969f83125e1c4e5b0
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-05-22 17:04:21 -0300

    cpp is weird

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ const char WALL = '#';
 const char FLOOR = '.';
 const int WIDTH = 10;
 const int HEIGHT = 10;
+const int MAX_MAP_SIZE = WIDTH * HEIGHT;
 
 struct tile {
     int x, y;
@@ -22,16 +23,23 @@ int get_index(int x, int y) {
     return x + (WIDTH * y);
 }
 
-int main() {
-    tile *tiles[WIDTH * HEIGHT] = {};
+tile* initialize_map() {
+    static tile tiles[MAX_MAP_SIZE]{};
     for (int x = 0; x < WIDTH; x++) {
         for (int y = 0; y < HEIGHT; y++) {
             int i = get_index(x, y);
-            tiles[i] = new tile{x, y, static_cast<bool>(std::rand() % 2)};
-            std::cout << tiles[i]->return_tile();
+            tiles[i] = {x, y, true};
+            std::cout << tiles[i].return_tile();
         }
-		std::cout << '\n';
+        std::cout << '\n';
     }
 
+    return tiles;
+}
+
+int main() {
+    // pointer to an array
+    tile* tiles = initialize_map();
+    
     return 0;
 }
